6820162ab6f2d50596fd09dbbab7d251b2c3c54e
Fernando Davi <92491040+fnddavi@users.noreply.github.com>
2024-07-25 23:56:01 -0300

    altera 50

--- exercicios/50.py
+++ exercicios/50.py
@@ -2,3 +2,10 @@
 Desenvolva um programa que leia seis números inteiros e mostre a soma apenas daqueles que forem pares. Se o valor digitado for ímpar, desconsidere-o.
 '''
 
+nm=0
+for i in range(0, 6):
+    nm = int(input(f"{i+1}o Numero: "))
+
+if nm % 2 == 0:
+    soma = (nm+nm)
+    print(soma)
