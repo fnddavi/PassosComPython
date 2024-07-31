726dbdc73fc1b57c04f2de87a4e960a810e1c6f8
fnddavi <fernando.ferreira32@fatec.sp.gov.br>
2024-07-30 23:15:47 -0300

    up

--- exercicios/51.py
+++ exercicios/51.py
@@ -1,4 +1,18 @@
 '''
-Desenvolva um programa que leia o primeiro termo e a razão e uma PA. No final, mostre os 10 primeiros termos dessa progressão.
+Desenvolva um programa que leia o primeiro termo e a razão e uma PA.
+No final, mostre os 10 primeiros termos dessa progressão.
 '''
 
+print("\nVamos calcular uma PA simples. Abaixo digite o Primeiro termo e a razão!\n")
+ter = int(input('Primeiro termo: '))
+raz = int(input('Razão: '))
+
+termos = []
+cont = 0
+
+while cont < 10:
+    termos.append(ter)
+    ter += raz
+    cont +=1
+
+print(termos)
