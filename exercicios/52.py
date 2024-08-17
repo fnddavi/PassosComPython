122997f0562f335b1789b44eae0e4f410999c47f
Fernando Davi <92491040+fnddavi@users.noreply.github.com>
2024-08-17 11:03:35 -0300

    52

--- exercicios/52.py
+++ exercicios/52.py
@@ -2,3 +2,19 @@
 Faça um programa que leia um número inteiro e diga se ele é ou não um número primo.
 '''
 
+print("Verificaremos se o número que sugerir é primo ou não. Tente um número de 1 a 1000.")
+num = int(input("\nQual número? "))
+
+
+if num > 1:
+    primo = True
+    for i in range(2, int(num**0.5) + 1):
+        if num % i == 0:
+            primo = False
+            break
+    if primo:
+        print("Primo")
+    else:
+        print("NÃO Primo 2")
+else:
+    print("NÃO Primo 1")
