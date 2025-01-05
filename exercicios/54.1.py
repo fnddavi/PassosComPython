9bb0b017c579b58cb53fa82d65ee7f75817e552f
fnddavi <fernando.ferreira32@fatec.sp.gov.br>
2025-01-05 14:36:35 -0300

    até 54

--- exercicios/54.1.py
+++ exercicios/54.1.py
@@ -5,18 +5,13 @@ Crie um programa que leia o ano de nascimento de sete pessoas. No final, mostre 
 print(
     "Informe o ano de nascimento de sete pessoas para saber quantas já atingiram a maioridade e quantas ainda não.")
 
-anosGalera = []
+
 anosVerif = []
+for i in range(7):
+    anosInsert=int(input(f"{i+1}o ano de nascimento: "))
+    anosVerif.append(2025-anosInsert)
 
-anosInsert = int(input ("Digite o primeiro ano de nascimento: "))
-anosVerif.append(2025 - anosInsert)
-anosGalera.append(anosInsert)
-
-anosInsert = int(input ("Digite o segundo ano de nascimento: "))
-anosVerif.append(2025 - anosInsert)
-anosGalera.append(anosInsert)
-
-#print(anosGalera, anosVerif)
+print("Idades em ordem de inserção: ", anosVerif)
 
 for i in range(len(anosVerif)):
     if anosVerif[i] >= 18:
