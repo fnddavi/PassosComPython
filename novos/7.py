b75b17b28a27813de84c75ab0478e1ac3e9fb1a8
Fernando Davi <fernando.ferreira32@fatec.sp.gov.br>
2026-06-26 20:28:46 -0300

    ex 7 e 8

--- novos/7.py
+++ novos/7.py
@@ -1,1 +1,7 @@
- 
+vogals = "aeiouAEIOU"
+phrase = input("Enter a phrase: ")
+count = 0
+for char in phrase:
+    if char in vogals:
+        count += 1
+print(f"Number of vowels in the phrase: {count}")
